f1195570975b1527fc535644d495c610295a0bc4
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-12-01 14:10:18 -0600

    Actualizando password view

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,19 +1,49 @@
-import React  from 'react';
+import React, { useState } from 'react';
+
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 export const ForgotPassword = () => {
-    return(
+
+    const [form, setForm] = useState({
+        email: ''
+    });
+
+    const handleInputChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        const auth = getAuth();
+        sendPasswordResetEmail(auth, form.email)
+            .then(() => {
+                // Password reset email sent!
+                // ..
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
+
+    return (
         <div className="container py-5 h-100 w-100" id="div-form">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h4 className="fw-normal mb-3 pb-3 text-center">Restablecer contraseña</h4>
-                <label className="form-label text-center">Escriba su número de telefono o correo para recibir instrucciones para restablecer su contraseña.</label>
-                {/* input de correo o teléfono */}
+                <label className="form-label text-center">Escriba su correo para recibir instrucciones para restablecer su contraseña.</label>
+                {/* input de correo*/}
                 <div className="form-outline pt-5 mb-5 w-75 ">
-                    <label className="form-label" for="form2Example1">Correo electrónico o número de teléfono:</label>
-                    <input type="email" id="form2Example1" className="form-control" />
+                    <label className="form-label" for="form2Example1">Correo electrónico:</label>
+                    <input type="email" id="form2Example1" className="form-control" 
+                        value={form.email}
+                        onChange={handleInputChange} />
                 </div>
                 {/* botón */}
                 <div className="text-center">
-                    <button type="button" className="btn btn-primary btn-block mb-4">Enviar</button>
+                    <button type="submit" className="btn btn-primary btn-block mb-4">Enviar</button>
                 </div>
             </form>
         </div>
