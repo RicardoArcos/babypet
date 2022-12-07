e0d43fc5b9bbc39357f0e67ce98173fa1cc86666
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-12-07 15:59:43 -0600

    Añadiendo acerca de

--- src/components/AboutUsInfo.jsx
+++ src/components/AboutUsInfo.jsx
@@ -2,8 +2,26 @@ import React  from 'react';
 
 export const AboutUsInfo = () => {
     return (
-        <div className="container px4- py-5">
-            <h2 className="pb2- border-bottom">Babypet</h2>
-        </div>
+        <>        
+            <div className="container px-4 pt-5 mb-4">
+                <h2 className="pb-2 border-bottom">¿Quiénes somos?</h2>
+            </div>
+            <div className="container px-4 mb-4">
+                <h4 className="fw-bold mb-3">Misión</h4>
+                <p>Somos una empresa local con el propósito de mejorar la calidad de vida de tu mascota a través de la venta de productos de calidad para asegurar un sano desarrollo de nuestros pequeños amigos.</p>
+            </div>
+            <div className="container px-4 mb-4">
+                <h4 className="fw-bold mb-3">Visión</h4>
+                <p>Ser una empresa líder en la comercialización y distribución de productos para mascotas a nivel regional, para después ampliarnos a diversas ciudades y después a diversos estados. Colocar almacenes en localizaciones estratégicas para que los envíos de nuestros productos no se demoren. Todo esto ofreciendo los mejores productos con la calidad que tú mascota se merece.</p>
+            </div>
+            <div className="container px-4 mb-4">
+                <h4 className="fw-bold mb-3">Valores</h4>
+                <p>Nuestros clientes y sus mascotas deben de ser tratadas con todo el respeto y amabilidad que se merecen, nosotros somos gente que se caracteriza por ser empáticos, honestos y solidarios. Estos valores nos serán muy útiles y de gran importancia para poder conseguir nuestras metas y objetivos. </p>
+            </div>
+            <div className="container px-4 mb-4">
+                <h4 className="fw-bold mb-3">¿Necesitas ayuda con algo?</h4>
+                <p> Si tienes problemas, dudas o quieres darnos alguna recomendación, puedes llamarnos al 55-7468-2099 o envianos un correo a soporte.babypet@babypet.com </p>
+            </div>
+        </>
     );
 }
